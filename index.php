4274665409da4eff28badd594daaed393437ead3
MonteroSolisStephenDaniel <smontero25@ilb.cat>
2026-04-07 09:58:04 +0200

    aplicació d'estils diferents a files parelles i senars

--- index.php
+++ index.php
@@ -4,8 +4,14 @@ $numero = 7;
 <!DOCTYPE html>
 <html lang="ca">
 <head>
-    <meta charset="UTF-8">
-    <title>Taula de multiplicar</title>
+<meta charset="UTF-8">
+<title>Taula de multiplicar</title>
+
+<style>
+.fila-senar { background-color: #ffffff; }
+.fila-parell { background-color: #dddddd; }
+</style>
+
 </head>
 <body>
 
@@ -13,12 +19,15 @@ $numero = 7;
 
 <table border="1">
 <?php for ($i = 1; $i <= 10; $i++): ?>
-    <tr>
-        <td><?php echo $numero; ?></td>
-        <td>x <?php echo $i; ?></td>
-        <td>=</td>
-        <td><?php echo $numero * $i; ?></td>
-    </tr>
+<?php
+$classe = ($i % 2 === 0) ? 'fila-parell' : 'fila-senar';
+?>
+<tr class="<?php echo $classe; ?>">
+    <td><?php echo $numero; ?></td>
+    <td>x <?php echo $i; ?></td>
+    <td>=</td>
+    <td><?php echo $numero * $i; ?></td>
+</tr>
 <?php endfor; ?>
 </table>
 
